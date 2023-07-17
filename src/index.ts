0f6d3ae02bae37dd992f17af7758b890ac1babb5
Avni Genç <avnignc@gmail.com>
2023-07-17 20:41:49 +0300

    chore: export all types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,32 @@
 export { AuthenticationClient, OrderClient, StoreClient } from './clients';
-export { Scope } from './model/authentication/authenticaton.model';
+export { Authentication, AuthenticationResponse, GrantType, Scope } from './model/authentication/authenticaton.model';
+export {
+  AcceptOrder,
+  AcceptOrderResponse,
+  CancelOrderResponse,
+  DenyOrder,
+  DenyOrderResponse,
+  GetActiveCreatedOrders,
+  GetActiveCreatedOrdersResponse,
+  GetLatestCancelledOrders,
+  GetLatestCancelledOrdersResponse,
+  GetOrderDetails,
+  GetOrderDetailsResponse,
+  PatchCart,
+  PatchCartResponse,
+  UpdateDeliveryStatus,
+  UpdateDeliveryStatusResponse,
+} from './model/order/order.model';
+export {
+  GetRestaurantStatusResponse,
+  GetStore,
+  GetStoreHolidayHours,
+  GetStoreHolidayHoursResponse,
+  GetStoreResponse,
+  GetStores,
+  GetStoresResponse,
+  SetRestaurantStatus,
+  SetRestaurantStatusResponse,
+  SetStoreHolidayHours,
+  SetStoreHolidayHoursResponse,
+} from './model/store/store.model';
